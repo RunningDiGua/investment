88110f56c26abcb4561b3c8369e979d4fd67e294
tilenmac <changdong.wang@yiducloud.cn>
2020-05-15 00:25:27 +0800

    pro

--- src/main/java/com/tilen/investment/InvestmentApplication.java
+++ src/main/java/com/tilen/investment/InvestmentApplication.java
@@ -1,21 +1,14 @@
 package com.tilen.investment;
 
-import com.tilen.investment.common.JsonMapper;
-import java.io.IOException;
-import java.nio.charset.Charset;
-
-import com.tilen.investment.zhuanzhai.ZhuanZhaiHttpResp;
-import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class InvestmentApplication {
 
   public static void main(String[] args) throws IOException {
-    String toString = IOUtils.resourceToString("/test.txt", Charset.defaultCharset());
-    ZhuanZhaiHttpResp obj = JsonMapper.getObj(toString, ZhuanZhaiHttpResp.class);
-
     SpringApplication.run(InvestmentApplication.class, args);
   }
 }
